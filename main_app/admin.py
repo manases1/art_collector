f4bf078be3bd311ffadeefa0208c1953ee021b6d
Manases <manasesgil95@gmail.com>
2022-10-06 21:08:16 -0500

    updates

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artist, Art, Playlist
 
 # Register your models here.
+admin.site.register(Artist)
+admin.site.register(Art)
+admin.site.register(Playlist)
--- main_app/migrations/0004_art.py
+++ main_app/migrations/0004_art.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=150)),
                 ('length', models.IntegerField(default=0)),
-                ('artist', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='songs', to='main_app.artist')),
+                ('artist', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='arts', to='main_app.artist')),
             ],
         ),
     ]
